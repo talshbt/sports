05239ef97d600f8b08e8e96931f5286aa02a9168
talshbt <53658521+talshbt@users.noreply.github.com>
2021-09-17 16:05:13 +0300

    update

---
+++ src/main/java/com/jb/sports/SportsApplication.java
@@ -0,0 +1,14 @@
+package com.jb.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
