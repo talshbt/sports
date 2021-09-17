3f8468ad7b01b3944a383ef92f837b73ffc3aae4
talshbt <53658521+talshbt@users.noreply.github.com>
2021-09-17 16:43:14 +0300

    update

--- src/main/java/com/jb/sports/repos/PlayerRepository.java
+++ src/main/java/com/jb/sports/repos/PlayerRepository.java
@@ -3,5 +3,5 @@ package com.jb.sports.repos;
 import com.jb.sports.beans.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
+public interface PlayerRepository extends JpaRepository<Player, Long> {
 }
--- src/main/java/com/jb/sports/repos/TeamRepository.java
+++ src/main/java/com/jb/sports/repos/TeamRepository.java
@@ -3,5 +3,5 @@ package com.jb.sports.repos;
 import com.jb.sports.beans.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeamRepository extends JpaRepository<Team, Integer> {
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
